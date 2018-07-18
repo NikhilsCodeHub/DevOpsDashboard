5243f135a313102140fa17bae10cdcb3c3befab3
psg340b\nwaishampayan <nwaishampayan@psg340b.com>
2018-07-18 17:06:24 -0500

    added components
    sidebar.html
    gulpfile
    bootstrap

---
+++ gulpfile.js
@@ -0,0 +1,28 @@
+var gulp = require('gulp'),
+    concat = require('gulp-concat'),
+    uglify = require('gulp-uglify'),
+    rename = require('gulp-rename'),
+    sass = require('gulp-ruby-sass'),
+    autoprefixer = require('gulp-autoprefixer'),
+    browserSync = require('browser-sync').create();
+
+
+gulp.task('browser-sync', function() {
+    browserSync.init({
+        server: {
+            baseDir: './'
+        },
+        startPath: './app/index.html'
+    });
+});
+
+gulp.task('watch', function() {
+  // Watch .html files
+  gulp.watch('app/*.html', browserSync.reload);
+  gulp.watch('components/*.html', browserSync.reload);
+  // Watch .js files
+  gulp.watch('app/js/*.js', ['scripts']);
+});
+
+// Default Task
+gulp.task('default', ['browser-sync', 'watch']);

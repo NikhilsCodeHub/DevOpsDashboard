04cb233c7b9ad71e9cd5eae5451834cb598bb061
NikhilW <NikhilsCodeHub@users.noreply.github.com>
2018-08-02 08:11:23 -0500

    Updates to sidebar
    identified helper js files and internal function

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,9 @@
 var gulp = require('gulp'),
-    concat = require('gulp-concat'),
-    uglify = require('gulp-uglify'),
-    rename = require('gulp-rename'),
-    sass = require('gulp-ruby-sass'),
-    autoprefixer = require('gulp-autoprefixer'),
+    // concat = require('gulp-concat'),
+    // uglify = require('gulp-uglify'),
+    // rename = require('gulp-rename'),
+    // sass = require('gulp-ruby-sass'),
+    // autoprefixer = require('gulp-autoprefixer'),
     browserSync = require('browser-sync').create();
 
 
@@ -12,22 +12,14 @@ gulp.task('browser-sync', function() {
         server: {
             baseDir: './'
         },
-<<<<<<< HEAD
-<<<<<<< HEAD
         startPath: 'app/index.html'
-=======
-        startPath: './app/index.html'
->>>>>>> 3ad0f6d56c12bd20e881b07866c67bb927104280
-=======
-        startPath: './app/index.html'
->>>>>>> 3ad0f6d56c12bd20e881b07866c67bb927104280
     });
 });
 
 gulp.task('watch', function() {
   // Watch .html files
   gulp.watch('app/*.html', browserSync.reload);
-  gulp.watch('components/*.html', browserSync.reload);
+  gulp.watch('components/sidebar/*.html', browserSync.reload);
   // Watch .js files
   gulp.watch('app/js/*.js', ['scripts']);
 });
